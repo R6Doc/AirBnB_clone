9a05844db71f1db4767a06c65e7e00a27d564c77
R6Doc <fiendgear@gmail.com>
2020-06-25 12:44:09 -0500

    adding classes

---
+++ models/review.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class Review"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+	"""Review"""
+	place_id = ""
+	user_id = ""
+	text = ""
+
+	def __init__(self, *args, **kwargs):
+		"""Init Review"""
+		super().__init__(*arg, **kwargs)
---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""State Class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+	"""An State"""
+	def __init__(self, *args, **kwargs):
+		"""Init State"""
+		super().__init__(*args, **kwargs)
+		
