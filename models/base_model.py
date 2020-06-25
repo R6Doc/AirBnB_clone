e2a97b1ba3df1cb659ed860691d8e58b260dd8ad
Blazeker <1687@holbertonschool.com>
2020-06-25 17:03:31 +0000

    First steps

---
+++ models/base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+from datetime import datetime
+import uuid
+
+
+class BaseModel:
+    """ Class Base """
+
+    def __init__(self, id, created_at, updated_at):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = created_at
+
+    def __str__ (self):
+        """ Returns the string representation """
+        return "{[<>]} {(<>)} {<>} ".format(self.__class__.__name__,
+                                            self.id,
+                                            self.__dict__)
+
+    def save(self):
+        """ Update the updatet_at attribute """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        return (
+            {
+                "id":self.id,
+                
+
+            }
+        )
+
+
+
+
+print(BaseModel.__dict__)
+
+
