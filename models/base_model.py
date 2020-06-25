d14f35e348d34073a49c5c60f7dc5c2a4e2e0178
Blazeker <1687@holbertonschool.com>
2020-06-25 18:31:23 +0000

    First exercise

--- models/base_model.py
+++ models/base_model.py
@@ -2,37 +2,28 @@
 from datetime import datetime
 import uuid
 
-
 class BaseModel:
     """ Class Base """
 
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = created_at
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
     def __str__ (self):
         """ Returns the string representation """
-        return "{[<>]} {(<>)} {<>} ".format(self.__class__.__name__,
-                                            self.id,
-                                            self.__dict__)
+        return "[{}] ({}) {} ".format(self.__class__.__name__,
+                                        self.id,
+                                        self.__dict__)
 
     def save(self):
         """ Update the updatet_at attribute """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.today()
 
     def to_dict(self):
-        return (
-            {
-                "id":self.id,
-                
-
-            }
-        )
-
-
-
-
-print(BaseModel.__dict__)
-
+        my_dict = self.__dict__.copy()
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict["__class__"] = self.__class__.__name__
+        return my_dict
 
