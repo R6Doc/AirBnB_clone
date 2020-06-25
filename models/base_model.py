f7b6ae9158ead6ee0ff1098ed66155aa254f22b9
Blazeker <1687@holbertonschool.com>
2020-06-25 18:32:11 +0000

    Fixed pep8

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 import uuid
 
+
 class BaseModel:
     """ Class Base """
 
@@ -10,11 +11,11 @@ class BaseModel:
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
 
-    def __str__ (self):
+    def __str__(self):
         """ Returns the string representation """
         return "[{}] ({}) {} ".format(self.__class__.__name__,
-                                        self.id,
-                                        self.__dict__)
+                                      self.id,
+                                      self.__dict__)
 
     def save(self):
         """ Update the updatet_at attribute """
@@ -26,4 +27,3 @@ class BaseModel:
         my_dict["updated_at"] = self.updated_at.isoformat()
         my_dict["__class__"] = self.__class__.__name__
         return my_dict
-
