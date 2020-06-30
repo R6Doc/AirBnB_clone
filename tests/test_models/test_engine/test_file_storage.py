b83c02a5e336b07bf784c89c94758e3b848246c5
Blazeker <1687@holbertonschool.com>
2020-06-30 20:06:47 +0000

    Fixed

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Tests engine file"""
+
+import unittest
+
+
+class TestsBase(unittest.TestCase):
+    """Class to test the engine cases"""
+
+    def test_any(self):
+        """Test"""
+        pass
