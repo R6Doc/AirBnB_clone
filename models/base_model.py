c252d24f86bc2ce045793203e8e15426f596223f
R6Doc <fiendgear@gmail.com>
2020-06-30 15:19:37 -0500

    edited basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Module for base model """
 
-import models
 from datetime import datetime
-from uuid import uuid4
+import models
+import uuid
 
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
