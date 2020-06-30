0404d8dc4b4a661605dbe3fd34ee0c949b39d72c
Blazeker <1687@holbertonschool.com>
2020-06-30 15:20:38 +0000

    Some advanced

--- console.py
+++ console.py
@@ -75,13 +75,40 @@ class HBNBCommand(cmd.Cmd):
         """ Prints all the objects """
         aux = line.split()
         if not len(line):
-            print([objects for objects in storage.all().values()])
+            print([str(value) for key, value in storage.all().items()])
             return
         if aux[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        print([objects for objects in storage.all().values()
-              if strings[0] == objects.__class__.__name__])
+        print([str(value) for key, value in storage.all().items()
+              if aux[0] == type(value).__name__])
+
+    def do_update(self, line):
+        """ Update an instace """
+        aux = line.split()
+        if not len(line):
+            print("** class name missing **")
+            return
+        if aux[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        if len(aux) == 1:
+            print("** instance id missing **")
+            return
+        if len(aux) == 2:
+            print("** attribute name missing **")
+            return
+        if len(aux) == 3:
+            print("** value missing **")
+            return
+        keyWord = aux[0] + '.' + aux[1]
+        if keyWord not in storage.all().keys():
+            print("** no instance found **")
+            return
+        try:
+            setattr(storage.all()[keyWord], aux[2], eval(aux[3]))
+        except:
+            setattr(storage.all()[keyWord], aux[2], aux[3])
 
     def help_quit(self):
         """ Prints the help for the quit """
@@ -100,6 +127,16 @@ class HBNBCommand(cmd.Cmd):
         """ Exit the aplication """
         return True
 
+    def default(self, line):
+        """ Executes the process with the class name """
+        lineSplited = line.split(".")
+        className = lineSplited[0]
+        command = lineSplited[1].split('(')[0]
+        newLine = command + " " + className + " " + " "
+        string = lineSplited[1].split('(')[1].replace(')', "").split(", ")
+        argument = command + " " + className + " " + " " \
+            .join(el.replace('"', "") for el in string)
+        self.onecmd(argument)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
