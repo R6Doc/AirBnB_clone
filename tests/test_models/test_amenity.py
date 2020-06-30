c8e41d564af5d13a1e7ce2b3e2b5e83f6ab2ff26
Blazeker <1687@holbertonschool.com>
2020-06-30 19:26:17 +0000

    Test cases added

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Tests Base Model"""
+
+import unittest
+
+
+class TestsBase(unittest.TestCase):
+    """Class to test the amenity cases"""
+
+    def test_any(self):
+        """Test"""
+        pass
