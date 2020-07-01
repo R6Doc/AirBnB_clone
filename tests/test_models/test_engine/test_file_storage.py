65d021e8cb4dc02678e85792b0147324d04f742b
Blazeker <1687@holbertonschool.com>
2020-07-01 18:46:28 +0000

    File storage cases

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Tests file storage file """
+
+import unittest
+
+
+class TestsFileStorage(unittest.TestCase):
+    """ Class to test the file storage cases """
+
+    def test_any(self):
+        """Test"""
+        pass
